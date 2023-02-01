bfd570d9320ceed61ec60d9bfd6c19c9ed5bd6e0
Pavel Los <pashalos96@gmail.com>
2023-02-01 15:13:18 +0300

    Fraud check conteoller logging

--- fraud/src/main/java/com/pavellos/fraud/controller/FraudCheckHistoryController.java
+++ fraud/src/main/java/com/pavellos/fraud/controller/FraudCheckHistoryController.java
@@ -2,6 +2,7 @@ package com.pavellos.fraud.controller;
 
 import com.pavellos.fraud.dto.FraudCheckResponse;
 import com.pavellos.fraud.service.FraudCheckHistoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
+@Slf4j
 public record FraudCheckHistoryController(FraudCheckHistoryService fraudCheckHistoryService) {
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Long customerId){
+        log.info("Fraud check request for customer: {}", customerId);
         boolean isFraudCustomer = fraudCheckHistoryService.isFraudCustomer(customerId);
         return new FraudCheckResponse(isFraudCustomer);
     }
