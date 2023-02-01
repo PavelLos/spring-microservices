2c6f96022a6c0900d8f7c1d0695e853be155115a
Pavel Los <pashalos96@gmail.com>
2023-02-01 13:58:41 +0300

    New fraud microservice

---
+++ fraud/src/main/java/com/pavellos/fraud/FraudApp.java
@@ -0,0 +1,13 @@
+package com.pavellos.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(FraudApp.class, args);
+    }
+}
