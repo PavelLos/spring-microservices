415f9e3eec71f2014da472e1af4037939e5a6c51
Pavel Los <pashalos96@gmail.com>
2023-02-05 16:34:54 +0300

    Rabbit message producer

--- notification/src/main/java/com/pavellos/notification/NotificationApp.java
+++ notification/src/main/java/com/pavellos/notification/NotificationApp.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.pavellos.notification",
+                "com.pavellos.amqp"
+        }
+)
 @EnableFeignClients(basePackages = "com.pavellos.clients")
 public class NotificationApp
 {
